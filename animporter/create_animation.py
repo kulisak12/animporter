a71350ca892b71357e15cdf862617e9432d58387
kulisak12 <kulisak12lp@gmail.com>
2021-09-27 11:43:29 +0200

    Rework interpolation

--- animporter/create_animation.py
+++ animporter/create_animation.py
@@ -1,23 +1,46 @@
+import math
 from animporter.constants import TRANSITIONS
 
 
-class Frame:
+class State:
 	def __init__(self, pos, rot):
 		self.pos = pos
 		self.rot = rot
 
 def create_animation(out_dir, anim_path, timeline):
+	# synchronize animation begin and end
+	min_time = min([ keyframes[0]["time"] for keyframes in timeline.values() ])
+	for keyframes in timeline.values():
+		shift_time(keyframes, min_time)
+	max_time = min([ keyframes[len(keyframes) - 1]["time"] for keyframes in timeline.values() ])
+	last_frame_time = int(math.ceil(max_time))
+
 	interpolated = {}
 	for part, keyframes in timeline.items():
-		interpolated[part] = interpolate(keyframes)
-	pass
+		interpolated[part] = interpolate(keyframes, last_frame_time)
 
-def interpolate(keyframes):
-	# shift times to start at 0
+	frames = separate_frames(interpolated, last_frame_time + 1)
+
+
+# group states by frames
+# object of lists -> list of objects
+def separate_frames(state_lists, num_frames):
+	frames = [ {} for _ in range(num_frames) ]
+	for part, states in state_lists.items():
+		for i in range(num_frames):
+			frames[i][part] = states[i]
+	
+	return frames
+
+# shift times to start at 0
+def shift_time(keyframes, shift):
 	for f in keyframes:
-		f["time"] -= keyframes[0]["time"]
+		f["time"] -= shift
 
-	# calculate frames between keyframes
+
+def interpolate(keyframes, last_frame_time):
+
+	# calculate states between keyframes
 	interpolated = []
 	time = 0
 	for i in range(len(keyframes) - 1):
@@ -25,21 +48,21 @@ def interpolate(keyframes):
 		end = keyframes[i + 1]
 		transition = get_transition(begin["transition"])
 		while time < end["time"]:
-			interpolated.append(create_frame(begin, end, time, transition))
+			interpolated.append(create_state(begin, end, time, transition))
 			time += 1
-	# add last frame
+	# add last state
 	last_keyframe = keyframes[len(keyframes) - 1]
-	interpolated.append(Frame(last_keyframe["pos"], last_keyframe["rot"]))
-
-	return interpolated
+	last_states = [ State(last_keyframe["pos"], last_keyframe["rot"]) for _ in range(last_frame_time - time + 1) ]
+	
+	return interpolated + last_states
 	
 
-def create_frame(begin, end, time, transition):
-	new_frame = Frame(None, None)
+def create_state(begin, end, time, transition):
+	new_state = State(None, None)
 	percentage = (time - begin["time"]) / (end["time"] - begin["time"])
-	new_frame.pos = mix(begin["pos"], end["pos"], percentage, transition)
-	new_frame.rot = mix(begin["rot"], end["rot"], percentage, transition)
-	return new_frame
+	new_state.pos = mix(begin["pos"], end["pos"], percentage, transition)
+	new_state.rot = mix(begin["rot"], end["rot"], percentage, transition)
+	return new_state
 
 def mix(begin, end, percentage, transition):
 	return (1 - transition(percentage)) * begin + transition(percentage) * end
